ac2fa6f17ae1c5f5aa9869a245692bc08be4208a
Andres <jhohan.andres83@gmail.com>
2021-03-27 16:28:47 -0500

    asdss

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://andres:andres@cluster0.zcq3h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
